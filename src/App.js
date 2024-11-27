cea1732e9f5e30353a7aac2cbd37084f35c8a203
Atabayev Ali <sptmbrw@gmail.com>
2024-11-27 17:39:11 +0500

    Use js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { createElement } from "react";  // Императивный
+import logo from "./logo.svg";
+import "./App.css";
 
 export const App = () => {
-  const year = new Date().getFullYear();
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>{year}</div>
-      </header>
-    </div>
+  const year = new Date().getFullYear();  // Императивный
+
+  return createElement(
+    "div", // Декларативный
+    { className: "App" },
+    createElement(
+      "header",
+      { className: "App-header" },
+      createElement("img", {
+        src: logo,
+        className: "App-logo",
+        alt: "logo",
+      }),
+      createElement(
+        "p",
+        null,
+        "Edit ",
+        createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      ),
+      createElement("div", null, year) // Императивный
+    )
   );
-}
+};
